7107c89c8a5afd15fc7d46893cbaff3666294375
Aurélien Uguen <aurelienuguen@imac-de-aurelien.home>
2023-04-25 16:36:50 +0200

    Homepage done

--- _parts/footer.php
+++ _parts/footer.php
@@ -1,4 +1,9 @@
-
+        <footer>
+            <div class="container-fluid footer">
+                <h5 class="card-title">DonkeyEvent est une propriété de DonkeySchool.</h5>
+                <p class="card-text">Toute copie effectuée sans autorisation sera passible de 40 coups de fouet.</p>
+            </div>
+        </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     </body>
 </html>
--- _parts/home.php
+++ _parts/home.php
@@ -6,25 +6,27 @@
                 </div>
             </div>
 
-            <section class="container container-fluid">
-                    <h2 class="display-4">Que souhaitez-vous réserver ?</h2>
+            <section>
+                <div class="container container-fluid homebooking">
+                    <h2 class="display-4"><strong>Que souhaitez-vous réserver ?</strong></h2>
                     <div class="bloclink">
-                        <a href="#" class="d-flex flex-row mb-3">
+                        <a href="#" class="d-flex flex-row mb-3 nav-link linkcolor">
                             <img src="ressources/DonkeyEvent_Cinema.jpg" alt="CinemaPicture" width="200px" class="p-2">
-                            <div>
-                                <h3 class="p-2">Des places de cinéma</h3>
-                                <p>Réservez vos places pour le film de votre choix!</p>
+                            <div class="textbloc">
+                                <h3 class="p-2 display-5">Des places de cinéma.</h3>
+                                <p>Réservez vos places pour le film de votre choix !</p>
                             </div>
                         </a>
                     </div>
 
                     <div class="bloclink">
-                        <a href="#" class="d-flex flex-row mb-3">
+                        <a href="#" class="d-flex flex-row mb-3 nav-link linkcolor">
                             <img src="ressources/DonkeyEvent_Spectacle.jpg" alt="SpectaclePicture" width="200px" class="p-2">
-                            <div>
-                                <h3 class="p-2">Des places de spectacle</h3>
-                                <p>Réservez vos places pour un spectacle!</p>
+                            <div class="textbloc">
+                                <h3 class="p-2 display-5">Des places de spectacle.</h3>
+                                <p>Réservez vos places de spectacle !</p>
                             </div>
                         </a>
                     </div>
+                </div>
             </section>
