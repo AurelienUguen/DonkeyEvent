c3d5d2f178ff109b3573f490a7b1bbc4e9655012
Aurélien Uguen <aurelienuguen@imac-de-aurelien.home>
2023-04-25 15:35:46 +0200

    Addind first step of HTML structure with a bit of CSS/Bootstrap

---
+++ _parts/home.php
@@ -0,0 +1,30 @@
+            <div class="position-relative">
+                <img src="ressources/DonkeyEvent_Header_Light.webp" alt="DonkeyEvent_Header" class="img-fluid">
+                <div class="headertext position-absolute top-50 start-50 translate-middle">
+                    <h1 class="donkeyevent display-1">DonkeyEvent</h1>
+                    <p class="slogan h4">L'évènement à porté de main</p>
+                </div>
+            </div>
+
+            <section class="container container-fluid">
+                    <h2 class="display-4">Que souhaitez-vous réserver ?</h2>
+                    <div class="bloclink">
+                        <a href="#" class="d-flex flex-row mb-3">
+                            <img src="ressources/DonkeyEvent_Cinema.jpg" alt="CinemaPicture" width="200px" class="p-2">
+                            <div>
+                                <h3 class="p-2">Des places de cinéma</h3>
+                                <p>Réservez vos places pour le film de votre choix!</p>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="bloclink">
+                        <a href="#" class="d-flex flex-row mb-3">
+                            <img src="ressources/DonkeyEvent_Spectacle.jpg" alt="SpectaclePicture" width="200px" class="p-2">
+                            <div>
+                                <h3 class="p-2">Des places de spectacle</h3>
+                                <p>Réservez vos places pour un spectacle!</p>
+                            </div>
+                        </a>
+                    </div>
+            </section>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '_parts/header.php';
+require_once '_parts/home.php';
+require_once '_parts/footer.php';
+
+?>
+
