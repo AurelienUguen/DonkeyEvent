06fd183822d16b549d5f3cf238744a54c952e031
Aurélien Uguen <aurelienuguen@imac-de-aurelien.home>
2023-04-26 09:47:28 +0200

    Removing DS_Store file in ressource folder

--- cinema.php
+++ cinema.php
@@ -7,16 +7,17 @@ require_once 'templates/header.php';
 <section>
     <h1 class="display-4 title">Quel film souhaitez-vous voir ?</h1>
     <!-- Boucle foreach pour dupliquer la div ci-dessous pour chaque film -->
-    <div class="container container-fluid boxshow justify-content-center align-items-center">
+    <div class="container container-fluid boxshow">
         <img src="https://m.media-amazon.com/images/I/81-DmthvMwL.jpg" alt="" width="200px">
         <div class="blocinfoshow">
-            <h2 class="display-5">Les banshees d'Inisherin</h2>
+            <h2>Les banshees d'Inisherin</h2>
             <p class="h5">Sur Inisherin - une île isolée au large de la côte ouest de l'Irlande - deux compères de toujours, Pádraic et Colm, se retrouvent dans une impasse lorsque Colm décide du jour au lendemain de mettre fin à leur amitié.</p>
+            <div>
+                <a href="#">Voir plus</a>
+                <a href="#">Réserver</a>
+            </div>
         </div>
-        <div>
-            <a href="#">Voir plus</a>
-            <a href="#">Réserver</a>
-        </div>
+        
     </div>
 
 
