105ef5fd476f56dcbfa56010c21ffcb00dd71773
Aurélien Uguen <aurelienuguen@imac-de-aurelien.home>
2023-04-26 14:44:23 +0200

    adding details page

--- cinema.php
+++ cinema.php
@@ -8,7 +8,7 @@ require_once 'templates/header.php';
     <h1 class="display-4 title">Quel film souhaitez-vous voir ?</h1>
     <!-- Boucle foreach pour dupliquer la div ci-dessous pour chaque film -->
     <div class="container container-fluid masterbox">
-        <img src="https://m.media-amazon.com/images/I/81-DmthvMwL.jpg" class="img-fluid" alt="" width="200px">
+            <div class="col-md-2"><img src="https://m.media-amazon.com/images/I/81-DmthvMwL.jpg" class="img-fluid" alt=""></div>
         <div class="container container-fluid mastertextbox">
             <div class="infotextbox">
                 <h2>Les banshees d'Inisherin</h2>
--- show.php
+++ show.php
@@ -5,7 +5,16 @@ require_once 'templates/header.php';
 ?>
 
 <section>
-
+    <h1 class="display-4 title">Détails du film "Les Banshees d'Inisherin"</h1>
+    <div class="container container-fluid detailbox">
+        <div class="col-md-3"><img src="https://m.media-amazon.com/images/I/81-DmthvMwL.jpg" class="img-fluid" alt=""></div>
+        <div class="container container-fluid moredetails">
+            <h2 class="display-3">Les Banshees d'Inisherin</h2>
+            <h3 class="display-5">De Martin McDonagh</h3>
+            <h4>Comédie dramatique</h4>
+            <p>Sur Inisherin - une île isolée au large de la côte ouest de l'Irlande - deux compères de toujours, Pádraic et Colm, se retrouvent dans une impasse lorsque Colm décide du jour au lendemain de mettre fin à leur amitié. Abasourdi, Pádraic n’accepte pas la situation et tente par tous les moyens de recoller les morceaux, avec le soutien de sa sœur Siobhan et de Dominic, un jeune insulaire un peu dérangé. Mais les efforts répétés de Pádraic ne font que renforcer la détermination de son ancien ami et lorsque Colm finit par poser un ultimatum désespéré, les événements s’enveniment et vont avoir de terribles conséquences.</p>
+        </div>
+    </div>
 
 
 </section>
